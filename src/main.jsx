905b9dc728bbe47fbbef63f4616691ced17a656e
Abhay Singh Tomar <abhaysinghtomar97@gmail.com>
2026-02-14 21:29:52 +0530

    debug

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import AstralApp from './astral'
+import AstralApp from './Astral'
 
 
 createRoot(document.getElementById('root')).render(
